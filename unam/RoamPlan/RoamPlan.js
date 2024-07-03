e69e785f145c2083641f1aeb54f4bdc79cac1a65
Jacob Hoopes <rjacobhoopes@gmail.com>
2024-07-03 08:17:14 -0700

    Website updates 3 July

--- unam/RoamPlan/RoamPlan.js
+++ unam/RoamPlan/RoamPlan.js
@@ -1,3 +1,5 @@
+
+
 let resizeTimeout;
 
 function onWindowResize() {
@@ -14,13 +16,6 @@ function debounceResize() {
 
 window.addEventListener('resize', debounceResize)
 
-// document.addEventListener('click')
-
-// function testing(item) {
-//     // let object = document.getElementById(item);
-//     // object.style.backgroundColor = "red";
-
-// }
 
 const testing = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
@@ -31,19 +26,18 @@ const testing = () => {
     }
 }
 
-img.addEventListener('click', testing);
-testing();
+
 
 var currentMap = 0;
 
 
-const changeMap = () => {
 
-    
+const changeMap = () => {
     currentMap = (currentMap + 1) % 5;
     let juxtaposeMap = document.getElementById("jDiv");
     // juxtaposeMap.style.border = "5px solid black";
     setTimeout(returnBackground, 500);
+    juxtaposeMap.
     juxtaposeMap.dataset.startingposition = Math.floor(Math.random()*100).toString() + "%";
     if (juxtaposeMap.children.length > 0) {
         const firstChild = juxtaposeMap.children[0];
@@ -52,24 +46,110 @@ const changeMap = () => {
         const script = document.createElement('script');
         script.src = "../../juxtapose/js/juxtapose.js?v=" + new Date().getTime();
         document.head.appendChild(script);
-
     }
-
-
 }
 
-change.addEventListener('click', changeMap)
+
+
+
 
 function returnBackground() {
     document.getElementById("jDiv").style.border = "0px"
 }
 
-jDiv.addEventListener('mousemove', () => {
-    let juxtaposeMap = document.getElementById("jDiv");
 
-    document.getElementById("color").innerHTML = juxtaposeMap.children[0];
-    // juxtaposeMap.setAttribute(leftPercent="10%") = Math.floor(Math.random()*100).toString() + "%";
-    juxtaposeMap.style.border = "2px black solid"
+// jDiv.addEventListener('mousemove', () => {
+//     let juxtaposeMap = document.getElementById("jDiv");
 
-    setTimeout(returnBackground, 500);
-});
+//     document.getElementById("color").innerHTML = juxtaposeMap.children[0];
+//     // juxtaposeMap.setAttribute(leftPercent="10%") = Math.floor(Math.random()*100).toString() + "%";
+//     juxtaposeMap.style.border = "2px black solid"
+
+//     setTimeout(returnBackground, 500);
+// });
+
+
+
+// var slide = document.getElementById("Zoom");
+// var zoomLevel = document.getElementById("zoomLevel");
+// zoomLevel.innerHTML = slide.value; // Display the default slider value
+
+
+// var currentLocation = document.getElementById("currentLocation");
+// var location1 = document.getElementById("location1");
+// var location2 = document.getElementById("location2");
+// var location3 = document.getElementById("location3");
+
+// location1.onclick = function() {
+//     currentLocation.innerHTML = this.innerHTML;
+// }
+
+// location2.onclick = function() {
+//     currentLocation.innerHTML = this.innerHTML;
+// }
+
+// location3.onclick = function() {
+//     currentLocation.innerHTML = this.innerHTML;
+// }
+
+window.onload = function() {
+    cropSlider.oninput = function() {
+        var image = document.getElementById("img2");
+        image.style.clipPath = `polygon(0% 0%, ${(this.value) + "%"} 0%, ${(this.value) + "%"} 100%, 0% 100%)`
+
+        var text = document.getElementById("cropText");
+        text.innerHTML = `${(this.value) + "%"}`;
+    }
+    // var img = document.getElementById("img2");
+    // var s = document.getElementById("slider")
+    // var sliders = document.querySelectorAll('.slider');
+    // sliders.forEach(function(slider) {
+    //     var text = document.getElementById(slider.id + "Text");
+    //     text.innerHTML = slider.value;
+        
+    //     slider.oninput = function() {
+    //         var image = document.getElementById("img2");
+    //         image.style.clipPath = `polygon(0% 0%, ${(this.value) + "%"} 0%, ${(this.value) + "%"} 100%, 0% 100%)`
+    //         text.innerHTML = `${(this.value) + "%"}`;
+    //     }
+    // });
+
+    // sliders.oninput = function() {
+
+    //     img.style.clip = `polygon()`
+    //     //`rect(${calc(this.value*img.height/2)})`
+    // }
+
+}
+
+// Zoom.addEventListener('mousemove', () => {
+//     var text = document.getElementById("zoomLevel");
+//     text.innerHTML = Zoom.value;
+// })
+
+// window.onload = function() {
+//     var img = document.getElementById("img2");
+//     var slider = document.getElementById("Zoom");
+
+//     var sliders = document.querySelectorAll('.slider');
+//     sliders.forEach(function(slider) {
+//         var text = document.getElementById(slider.id + "Text");
+//         text.innerHTML = slider.value;
+        
+//         slider.oninput = function() {
+//             img.style.width = this.value + "%";
+//             text.innerHTML = this.value;
+//         }
+//     });
+//     sliders.oninput = function() {
+
+//         // img.style.clip = `rect(${calc(this.value*img.height/2)})`
+//     }
+
+// }
+
+Year1.addEventListener('mousemove', () => {
+    var element = document.getElementById("year1");
+    var setElement = document.getElementById("location_");
+    setElement.innerHTML = "THIs"
+})
